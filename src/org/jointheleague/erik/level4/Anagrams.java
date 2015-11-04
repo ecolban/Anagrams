8468e241d3b3fcb984b083610821462df92ecbf5
ecolban <erik.colban@drawmetry.com>
2015-11-03 16:01:11 -0800

    New approach using a map to look up anagrams

--- src/org/jointheleague/erik/level4/Anagrams.java
+++ src/org/jointheleague/erik/level4/Anagrams.java
@@ -2,22 +2,63 @@ package org.jointheleague.erik.level4;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URISyntaxException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.nio.CharBuffer;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import javax.swing.JOptionPane;
 
 public class Anagrams {
 
-    public static void main(String[] args) throws IOException, URISyntaxException {
-        new Anagrams().run();
+    private Map<String, List<String>> map;
+
+    public Anagrams(InputStream wordSource) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(wordSource))) {
+            map = reader.lines().collect(Collectors.groupingBy(Anagrams::getRep));
+        }
     }
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        Anagrams anagrams = new Anagrams(Anagrams.class.getResourceAsStream("res/words.txt"));
+        List<List<String>> classes = new ArrayList<>(anagrams.map.values());
+        classes.sort((c1, c2) -> c2.size() - c1.size());
+        classes.stream().limit(20).forEach(System.out::println);
+        anagrams.run();
+    }
+
+    private void run() throws IOException, URISyntaxException {
+
+        for (boolean play = true; play;) {
+            String w = JOptionPane.showInputDialog("Which word?");
+            String msg = (w == null ? "No anagrams." : "Anagrams: " + findAnagrams(w))
+                    + "\nTry again?";
+            int answer = JOptionPane.showConfirmDialog(null, msg, "Anagrams",
+                    JOptionPane.YES_NO_OPTION);
+            play = answer == JOptionPane.YES_OPTION;
+        }
+    }
+
+    public static String getRep(String s) {
+        char[] b = s.toLowerCase().toCharArray();
+        Arrays.sort(b);
+        return new String(b);
+    }
+
+    public String findAnagrams(String word) {
+        String key = getRep(word);
+        if (map.containsKey(key)) {
+            return map.get(key).stream().collect(Collectors.joining(", "));
+        } else {
+            return "";
+        }
+    }
+
+    /* Three different implementations of the isAnagram() method */
 
     public static boolean isAnagram1(String word1, String word2) {
         if (word1 != null && word2 != null && word1.length() == word2.length()) {
@@ -58,39 +99,6 @@ public class Anagrams {
         } else {
             return false;
         }
-    }
-
-    public static String getRep(String s) {
-        char[] b = s.toLowerCase().toCharArray();
-        Arrays.sort(b);
-        return new String(b);
-    }
-
-    private void run() throws IOException, URISyntaxException {
-        URLConnection connection = new URL(
-                "https://raw.githubusercontent.com/eneko/data-repository/master/data/words.txt")
-                        .openConnection();
-        List<String> words;
-        try (BufferedReader reader = new BufferedReader(
-                new InputStreamReader(connection.getInputStream()))) {
-            words = reader.lines().collect(Collectors.toList());
-        }
-        for (boolean play = true; play;) {
-            String w = JOptionPane.showInputDialog("Which word?");
-            String msg = w == null
-                    ? "No anagrams."
-                    : "Anagrams: " + findAnagrams(w, words) + "\nTry again?";
-            int answer = JOptionPane.showConfirmDialog(null, msg, "Anagrams",
-                    JOptionPane.YES_NO_OPTION);
-            play = answer == JOptionPane.YES_OPTION;
-        }
-    }
-
-    private String findAnagrams(String word, List<String> words)
-            throws IOException {
-        return words.stream()
-                .filter(w -> Anagrams.isAnagram2(word, w))
-                .collect(Collectors.joining(", "));
     }
 
 }
--- test/org/jointheleague/erik/level4/AnagramsTest.java
+++ test/org/jointheleague/erik/level4/AnagramsTest.java
@@ -1,10 +1,52 @@
 package org.jointheleague.erik.level4;
-import static org.junit.Assert.*;
 
-import org.jointheleague.erik.level4.Anagrams;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Test;
 
-public class AnagramsTest {
+import junit.framework.TestCase;
+
+public class AnagramsTest extends TestCase {
+
+    Anagrams instance;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        List<String> words = Arrays.asList(
+                "acts", "cast",
+                "grounded", "underdog", "undergod",
+                "post", "spot", "stop", "tops");
+        InputStream wordSource = new ByteArrayInputStream(stringsToBytes(words));
+        instance = new Anagrams(wordSource);
+    }
+
+    public byte[] stringsToBytes(List<String> words) throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        byte newline = (byte) '\n';
+        for (String w : words) {
+            baos.write(w.getBytes());
+            baos.write(newline);
+        }
+        return baos.toByteArray();
+    }
+
+    @Test
+    public void testGetRep() {
+        assertEquals("aelst", Anagrams.getRep("steal"));
+        assertEquals("aailmn", Anagrams.getRep("animal"));
+        assertEquals("ddegnoru", Anagrams.getRep("grounded"));
+        assertEquals("beggir", Anagrams.getRep("bigger"));
+        assertEquals("aellmrs", Anagrams.getRep("smaller"));
+        assertEquals("beggir", Anagrams.getRep("bigger"));
+        assertEquals("aelqu", Anagrams.getRep("equal"));
+        assertEquals("ellsw", Anagrams.getRep("swell"));
+    }
 
     @Test
     public void testIsAnagramPositive() {
@@ -12,7 +54,7 @@ public class AnagramsTest {
         assertTrue(Anagrams.isAnagram2("stale", "slate"));
         assertTrue(Anagrams.isAnagram2("aabbb", "babab"));
     }
-    
+
     @Test
     public void testIsAnagramNegative() {
         assertFalse(Anagrams.isAnagram2("apple", "banana"));
@@ -21,18 +63,19 @@ public class AnagramsTest {
         assertFalse(Anagrams.isAnagram2("aabbb", "babbb"));
         assertFalse(Anagrams.isAnagram2("loooooong", "shrt"));
     }
+
     @Test
     public void testIsAnagramCaseSensitivity() {
         assertTrue(Anagrams.isAnagram2("STALE", "Slate"));
         assertTrue(Anagrams.isAnagram2("BANANA", "baNanA"));
     }
-    
+
     @Test
     public void testIsAnagramWithEmptyString() {
         assertFalse(Anagrams.isAnagram2("banana", ""));
         assertFalse(Anagrams.isAnagram2("", "banana"));
         assertTrue(Anagrams.isAnagram2("", ""));
-        
+
     }
 
     @Test
@@ -44,4 +87,33 @@ public class AnagramsTest {
         assertFalse(Anagrams.isAnagram2(null, null));
     }
 
+    @Test
+    public void testFindAnagramsOfNonWord() {
+        assertTrue(instance.findAnagrams("strmpf").isEmpty());
+    }
+
+    @Test
+    public void testAnagramsOfSpot() {
+        String anagrams = instance.findAnagrams("spot");
+        assertTrue(anagrams.lastIndexOf("stop") != -1);
+        assertTrue(anagrams.lastIndexOf("post") != -1);
+        assertTrue(anagrams.lastIndexOf("tops") != -1);
+        assertTrue(anagrams.lastIndexOf("spot") != -1);
+    }
+
+    @Test
+    public void testAnagramsOfGrounded() {
+        String anagrams = instance.findAnagrams("grounded");
+        assertTrue(anagrams.lastIndexOf("underdog") != -1);
+        assertTrue(anagrams.lastIndexOf("undergod") != -1);
+    }
+
+    @Test
+    public void testAnagramsOfCats() {
+        String anagrams = instance.findAnagrams("cats").toLowerCase();
+        assertTrue(anagrams.lastIndexOf("acts") != -1);
+        assertTrue(anagrams.lastIndexOf("cast") != -1);
+        assertTrue(anagrams.lastIndexOf("scat") == -1);
+        assertTrue(anagrams.lastIndexOf("cats") == -1);
+    }
 }
